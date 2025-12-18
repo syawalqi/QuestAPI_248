85ec80184b3d3a65ec47eae040ee3e9c3193be1e
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-12-18 09:12:21 +0700

    membuat ServiceApiSiswa.kt import

--- app/src/main/java/com/example/questapi_248/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_248/apiservice/ServiceApiSiswa.kt
@@ -1,5 +1,10 @@
 package com.example.questapi_248.apiservice
 
+import com.example.questapi_248.modeldata.DataSiswa
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
 interface ServiceApiSiswa {
     @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
