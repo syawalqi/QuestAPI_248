25524f3ac98914a06ca8ab3163dddb621ec15654
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-12-18 09:05:00 +0700

    membuat ServiceApiSiswa.kt / @GET / @POST

--- app/src/main/java/com/example/questapi_248/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_248/apiservice/ServiceApiSiswa.kt
@@ -3,5 +3,7 @@ package com.example.questapi_248.apiservice
 interface ServiceApiSiswa {
     @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
-)
+
+    @POST("insertTM.php")
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 }
