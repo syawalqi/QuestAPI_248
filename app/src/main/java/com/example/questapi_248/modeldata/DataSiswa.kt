265217b02923a80bb0141e96a3147bfafd3bf713
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-12-18 09:00:57 +0700

    membuat DataSiswa.kt / @Serializable / data class datasiswa

--- app/src/main/java/com/example/questapi_248/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/questapi_248/modeldata/DataSiswa.kt
@@ -1,2 +1,11 @@
 package com.example.questapi_248.modeldata
 
+@Serializable
+data class DataSiswa(
+    val id : Int,
+    val nama : String,
+    val alamat : String,
+    val telpon : String,
+
+
+)
