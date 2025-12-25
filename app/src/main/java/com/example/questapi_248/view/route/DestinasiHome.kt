2ec528763fff4d4fb2df14e86fd655b76a8c0a09
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-12-25 17:37:59 +0700

    update DestinasiHome.kt

--- app/src/main/java/com/example/questapi_248/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/questapi_248/view/route/DestinasiHome.kt
@@ -1,8 +1,9 @@
-package com.example.questapi_248.uicontroller.route
+package com.example.questapi_248.view.route
 
 import com.example.questapi_248.R
 
 object DestinasiHome : DestinasiNavigasi {
+
     override val route = "home"
     override val titleRes = R.string.app_name
 }
