1cc609d8ff48c1eee0e82a0d69a4ba2448ba4b2f
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-12-25 14:14:29 +0700

    menambah DestinasiNavigasi.kt, DestinasiHome.kt, DestinasiEntry.kt

---
+++ app/src/main/java/com/example/questapi_248/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.questapi_248.uicontroller.route
+
+import com.example.questapi_248.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
---
+++ app/src/main/java/com/example/questapi_248/uicontroller/route/DestinasiNavigasi.kt
@@ -0,0 +1,6 @@
+package com.example.questapi_248.uicontroller.route
+
+interface DestinasiNavigasi {
+    val route: String
+    val titleRes: Int
+}
