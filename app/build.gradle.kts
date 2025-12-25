081153ce7712a9affdebcc90cb7b0e141ace12b0
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-12-25 17:37:08 +0700

    membnarkan build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.okhttp)
     implementation(libs.okhttp.logging)
+    implementation(libs.androidx.compose.foundation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
