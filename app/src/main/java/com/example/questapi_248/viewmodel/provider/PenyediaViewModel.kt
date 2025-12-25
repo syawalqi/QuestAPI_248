56849e63989677d5622b486c43848123282dbbbf
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-12-25 17:39:53 +0700

    update PenyediaViewModel.kt

--- app/src/main/java/com/example/questapi_248/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questapi_248/viewmodel/provider/PenyediaViewModel.kt
@@ -1,16 +1,16 @@
 package com.example.questapi_248.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.questapi_248.repositori.AplikasiDataSiswa
+import com.example.questapi_248.viewmodel.DetailViewModel
+import com.example.questapi_248.viewmodel.EditViewModel
 import com.example.questapi_248.viewmodel.EntryViewModel
 import com.example.questapi_248.viewmodel.HomeViewModel
 
-/**
- * Extension untuk mengambil Application dari CreationExtras
- */
 fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
     this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]
             as AplikasiDataSiswa
@@ -27,6 +27,20 @@ object PenyediaViewModel {
 
         initializer {
             EntryViewModel(
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
+
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
+
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
                 aplikasiDataSiswa().container.repositoryDataSiswa
             )
         }
