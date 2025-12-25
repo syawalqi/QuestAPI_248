f8e34a66e3c64eaa7576e77ad0c20e84e73c46fb
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-12-25 17:40:19 +0700

    update ServiceApiSiswa.kt

--- app/src/main/java/com/example/questapi_248/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_248/apiservice/ServiceApiSiswa.kt
@@ -1,14 +1,37 @@
 package com.example.questapi_248.apiservice
 
 import com.example.questapi_248.modeldata.DataSiswa
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
 interface ServiceApiSiswa {
-    @GET("bacaTeman.php")
+
+    @GET("bacateman.php")
     suspend fun getSiswa(): List<DataSiswa>
 
     @POST("importTM.php")
-    suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+    suspend fun postSiswa(
+        @Body dataSiswa: DataSiswa
+    ): Response<Void>
+
+    @GET("baca1teman.php")
+    suspend fun getSatuSiswa(
+        @Query("id") id: Int
+    ): DataSiswa
+
+    @PUT("editTM.php")
+    suspend fun editSatuSiswa(
+        @Query("id") id: Int,
+        @Body dataSiswa: DataSiswa
+    ): Response<Void>
+
+    @DELETE("deleteTM.php")
+    suspend fun hapusSatuSiswa(
+        @Query("id") id: Int
+    ): Response<Void>
 }
