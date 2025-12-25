15d3ff3a77f0c7c1f3f7cac09ce14899b8836913
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-12-25 14:15:48 +0700

    fix MainActivity.kt

--- app/src/main/java/com/example/questapi_248/MainActivity.kt
+++ app/src/main/java/com/example/questapi_248/MainActivity.kt
@@ -7,41 +7,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.questapi_248.ui.theme.QuestAPI_248Theme
+import com.example.questapi_248.uicontroller.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             QuestAPI_248Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestAPI_248Theme {
-        Greeting("Android")
     }
 }
